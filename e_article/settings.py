4d35a24cfecd6708a488a7582fe7eb7b7aa2f56b
joelmanibi <85547782+joelmanibi@users.noreply.github.com>
2021-07-31 01:33:02 +0200

    Update settings.py

--- e_article/settings.py
+++ e_article/settings.py
@@ -134,7 +134,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
         os.path.join(PROJECT_ROOT,'static'),
     )
 
-    STATICFILES_STORAGE ='whitenoise.storage.CompresseManofestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
