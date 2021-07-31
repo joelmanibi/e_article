e54c685156bddba7ba4a1e0e3033e0458a18e755
joelmanibi <bi.mani@uvci.edu.ci>
2021-07-31 16:47:59 +0200

    Update settings.py

--- e_article/settings.py
+++ e_article/settings.py
@@ -140,5 +140,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 MEDIA_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
